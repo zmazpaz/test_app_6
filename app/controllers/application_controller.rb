c07e497041fd23a7197eb510ce88ce7f294a1b55
Aleksandrs Korcevskis <aleksandr.korcevski@zoho.com>
2020-08-31 22:16:47 +0100

    Initial commit and add new route

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,7 @@
+class ApplicationController < ActionController::Base
+  # def hello
+  #   render html: 'hello, world!' #This is not the preferred way!
+  # end
+
+  
+end
---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,4 @@
+class PagesController < ApplicationController
+  def home # no code needed here. It will expect views->pages->home.html.erb
+  end
+end
