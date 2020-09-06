cbb731c74a56d17fe4fbf1a3a1ac21e7af81d859
Aleksandrs Korcevskis <aleksandr.korcevski@zoho.com>
2020-09-06 21:03:05 +0100

    Modify Readme and routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   # root "application#hello" # NOT application_controller! #hello is the method in the controller we want to point to
-
+  #
   root 'pages#home'
 end
